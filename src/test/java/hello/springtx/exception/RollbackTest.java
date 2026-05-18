175fd8faffdaa943fbab615f5621946714a822e4
박준유 <pjy008008@naver.com>
2026-05-18 17:35:34 +0900

    chore: 불필요한 import문 삭제

--- src/test/java/hello/springtx/exception/RollbackTest.java
+++ src/test/java/hello/springtx/exception/RollbackTest.java
@@ -1,7 +1,6 @@
 package hello.springtx.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
