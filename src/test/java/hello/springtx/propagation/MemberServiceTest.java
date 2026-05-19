8584bfd288618519bf6735dcdfe8b0a292bdd864
박준유 <pjy008008@naver.com>
2026-05-19 09:45:37 +0900

    test: MemberService 테스트 코드 추가
    로그 저장 과정에서 예외가 생길 경우 테스트 코드 추가. 현재 데이터 정합성에 문제가 있는 코드임(member와 log가 둘 다 commit 되거나 rollback 되어야 하기 때문).

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,11 +1,11 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -35,5 +35,24 @@ class MemberServiceTest {
         //then
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService     @Transactional:OFF
+     * memberRepository  @Transactional:ON
+     * logRepository     @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
